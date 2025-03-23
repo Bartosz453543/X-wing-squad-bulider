4e43cb8f4b1d3cad71d5de34539230fde82aa52c
Bartosz453543 <bartoszgorczynski41@gmail.com>
2025-03-23 13:35:53 +0100

    ulepszenia dla ahsoki + poprawa ulepszeń u innych

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona główna</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h2>Stona główna</h2><br>
+        <p>Edytor eskadr do X wing 2.5</p>
+        
+    </header>
+    <main>
+    <section class="content">
+        <h2>Wyniki:</h2>
+
+    </section>
+    </main>
+
+</body>
+</html>
